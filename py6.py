f28b2a023ad87d6d9e17662f996bd303a6de6157
Alisa Choi <cojalisa072@gmail.com>
2025-09-07 10:09:13 +0300

    Add files via upload

--- py6.py
+++ py6.py
@@ -1,23 +1,27 @@
 password = input('Введите пароль: ')
 
+
 def sumlength(password):
-    if len(password) > 11:
-        return True
-    return False
+    return(len(password) > 11)
+
 
 def has_digit(password):
-    return any(letter.isdigit() for letter in password)
+    return any(has_digit.isdigit() for has_digit in password)
 
-def has_lower_letters(password):
+
+def has_lower_letters(password): 
     return any(has_lower_letters.islower() for has_lower_letters in password)
+
 
 def has_upper_letters(password):
     return any(has_upper_letters.isupper() for has_upper_letters in password)
 
-def has_symbols(password):
-    return any(has_symbols.isalnum() for has_symbols in password)
 
-def main(password):
+def has_symbols(password):
+    return any(not has_symbols.isalnum() for has_symbols in password)
+
+
+def main():
     sum_func = 0
     check = [
         sumlength(password),
@@ -25,10 +29,10 @@ def main(password):
         has_lower_letters(password), 
         has_upper_letters(password),
         has_symbols(password)
-        ]
+    ]
     for function in check:
         if function == True:
             sum_func += 2
     print('Рейтинг пароля', sum_func)
 if __name__ == '__main__':
-    main(password)
+    main()
